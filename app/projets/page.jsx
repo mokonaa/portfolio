c398fdb6fbaa22cea4bb7753eaf4e38d4ab63816
“Aline <hy.aline@outlook.com>
2025-04-29 10:00:57 +0200

    fix footer on all projects page and the single page component

--- app/projets/page.jsx
+++ app/projets/page.jsx
@@ -6,17 +6,20 @@ import Box from "@/components/Box";
 
 export default function page() {
     return (
-        <div className='allprojets'>
-            <Link href="/" passHref>
-                <h1 className='titre'>Aline Hy</h1>
-            </Link>
-            <Breadcrumb />
-            <div className="allprojets__container">
-                {projets.map((projet, index) => (
-                    <Box href={`/projets/${projet.slug}`} key={index} className={`${projet.slug} allprojets__box`} backgroundDesktop={projet.images[0]} backgroundMobile={projet.images[0]} imageAlt="Cliquez pour voir le projet" title={projet.titre}/>
-                ))}
+        <>
+            <div className='allprojets'>
+                <Link href="/" passHref>
+                    <h1 className='titre'>Aline Hy</h1>
+                </Link>
+                <Breadcrumb />
+                <div className="allprojets__container">
+                    {projets.map((projet, index) => (
+                        <Box href={`/projets/${projet.slug}`} key={index} className={`${projet.slug} allprojets__box`} backgroundDesktop={projet.images[0]} backgroundMobile={projet.images[0]} imageAlt="Cliquez pour voir le projet" title={projet.titre} />
+                    ))}
+                </div>
             </div>
             <Footer />
-        </div>
+        </>
+
     )
 }
